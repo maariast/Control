0b402f0a6f0f65df3d71a65d3ce536193d23199d
Alejandra Arias <maariast@unal.edu.co>
2019-05-17 17:46:44 -0500

    V2

--- LevitadorTReal.ino
+++ LevitadorTReal.ino
@@ -1,35 +1,43 @@
 
 #include "TimerOne.h"
 
+// Constantes físicas
 
-// constantes del PID
+float g=9.8;                 //kg*m/s^2
+float Rho=1.2;               //kg/m^3
+float A=0.00196;             //para r=0.025m
+float Cd=20;                 //Coeficiente de Arrastre
+float m=0.04;                //masa=40g
+float alpha=0.5*Cd*Rho*A; 
 
-
-float u=0;
-float x1=0, x1n=0;
-float x2=0, x2n=0;
-float Va=0, Van=0;
-int n=0;
-int Fs = 200;
-float D = (float)1/Fs;
-long Ts =1000000/Fs; 
-
-
-float alpha = 0.02356;
-float g= 9.8;
-float m =0.04;
+// Constantes Ventilador
 float k=1;
 float Thao=0.1;
 
-float r = 1;
-float I=0, Iant=0;
-float kp = 2;
-float ki = 1;
-float kib = ki*D;
-float e = 0;
-float P=0;
+// Variables de estado
 
-// filtro diseñado para wp = 0.1 en frecuencia normalizada 
+float x1=0, x1n=0;          //Posición        
+float x2=0, x2n=0;          //Velocidad  
+float Va=0, Van=0;          //Velocidad Aire  
+float u=0;                  //Entrada
+
+//Muestreo
+int n=0;
+int Fs = 200;
+float Delta = (float)1/Fs;
+long Ts =1000000/Fs;
+
+//Constantes PID
+  float ref = 1;
+  //Integral
+  float I=0, Iant=0; 
+  float ki = 1;
+  float kib = ki*Delta;
+  //Error  
+  float e = 0;
+  //Proporcional 
+  float kp = 2;
+    float P=0; 
 
 void setup() {
    Serial.begin(2000000);
@@ -39,52 +47,57 @@ void setup() {
     // timer para ajustar la interrupcion
     Timer1.initialize(Ts);        
     Timer1.attachInterrupt(control);
-
 }
-
-
-
 
 void control()
-{
+{   
+  
+    //Señal de referencia
     n+=1;
-
-   if (n % 2500==0){
-    if (r <5){
-      r =5 ;
-    }
-    else
-      r = 1;
-   } 
-    // Control 
-      
+    if (n % 2500==0){
+      if (ref <5){
+        ref = 5;
+      }else
+      ref = 1;
+    } 
     
-    e = r-x1;
+    // Control PI
+    e = ref-x1;
     P = kp*e;
     I = Iant+kib*e;
-    u = P+I; //P+I;
+    u = P+I;
+    /*
+    if(u>11){
+      u=12;
+    }else{
+      u = P+I; //P+I;
+    }
+    */
     
+    //Actualización de variables
     Iant = I; 
-        
-    // planta en lazo abierto
+
+    // Modelo de la planta
+    x1n=x1+Delta*x2;
+    x2n=x2+Delta*alpha/m*(Va-x2)*(Va-x2)-Delta*g; 
+    Van=Va*(1-Delta/Thao)+u*Delta*k/Thao; 
     
-    x1n = x2*D+x1; 
-    x2n = x2+D*alpha/m*(Va-x2)*(Va-x2)-D*g; 
-    Van = Va+D*k/Thao*u-Va*D/Thao;   
+    
     x1=x1n;
     x2=x2n;
-    Va=Van; 
- 
-if (n % 5==0){
+    Va=Van;  
 
-Serial.print(r,3);
-Serial.print('\t');
-Serial.println(x1,3);
-} 
-
+    if (n % 5==0){
+        Serial.print(ref,3);
+        Serial.print('\t');
+        //Serial.print(e,3);
+        Serial.print('\t');
+        Serial.println(x1,3);
+        Serial.print('\t');
+        //Serial.println(Va,3);
+    } 
 }
 
-
-// main loop no hace nada
-void loop(){
-  }
+//Main loop no ejecuta ninguna acción
+void loop() {
+}
